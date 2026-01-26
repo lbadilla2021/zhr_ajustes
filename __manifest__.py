9f238ca5d24e2c0e94d8b1d4dbe229c1d9cc9855
lbadilla2021 <lbadilla2021@gmail.com>
2026-01-26 12:36:13 -0300

    Add hr_skills dependency for resume fields

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Personalizaciones al Modulo HR',
     'version': '1.0',
-    'depends': ['hr'],
+    'depends': ['hr', 'hr_contract', 'hr_skills'],
     'data': [
         'security/ir.model.access.csv',
         'views/employee_field_usage_view.xml',
