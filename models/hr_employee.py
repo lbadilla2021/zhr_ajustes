f6c4780418c5a038e4b79181b99d0777d3b3d80b
lbadilla2021 <lbadilla2021@gmail.com>
2026-01-30 12:03:04 -0300

    Add cost center field to employee work info

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -8,3 +8,9 @@ class HrEmployee(models.Model):
     apellido_materno = fields.Char(string='Apellido Materno')
     nombres = fields.Char(string='Nombres')
     nombre_preferido = fields.Char(string='Nombre Preferido')
+    analytic_account_id = fields.Many2one(
+        'account.analytic.account',
+        string='Centro de Costo',
+        domain="[('plan_id.name', '=', 'Barca SpA')]",
+        help='Seleccione el centro de costo dentro del plan analítico de la empresa.',
+    )
