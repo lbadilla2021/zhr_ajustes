ffe4c2de8bdabd2a5c38e22a9d8369c6c670c085
lbadilla2021 <lbadilla2021@gmail.com>
2026-01-31 12:45:33 -0300

    Fix access control model references

---
+++ wizards/employee_termination_wizard.py
@@ -0,0 +1,25 @@
+from odoo import fields, models
+
+
+class HrEmployeeTerminationWizard(models.TransientModel):
+    _name = 'hr.employee.termination.wizard'
+    _description = 'Dar de Baja Empleado'
+
+    employee_id = fields.Many2one(
+        'hr.employee',
+        string='Empleado',
+        required=True,
+    )
+    departure_reason_id = fields.Many2one(
+        'hr.departure.reason',
+        string='Motivo de Salida',
+        required=True,
+    )
+
+    def action_confirm(self):
+        self.ensure_one()
+        self.employee_id.write({
+            'state': 'inactive',
+            'departure_reason_id': self.departure_reason_id.id,
+        })
+        return {'type': 'ir.actions.act_window_close'}
